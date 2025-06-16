d745b624e75a4e0322552b93110f80d49d4f6899
AnthonyJia <anthonyjiang117@gmail.com>
2025-06-16 16:58:14 -0400

    created the views and urls for portfolio app

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='portfolio-home'),
+    path('home/', views.home, name='portfolio-home'),
+    path('my-profile/', views.my_profile, name='portfolio-my-profile'),
+    path('<int: profile_id>/detail/', views.detail, name='portfolio-detail'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 
 def home(request):
     return render(request, 'portfolio/home.html')
+
+def my_profile(request):
+    return render(request, 'portfolio/my_profile.html')
+
+def detail(request, profile_id):
+    return render(request, 'portfolio/detail.html')
